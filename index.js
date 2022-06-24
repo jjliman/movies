c5a58abaa0ece28661dfcaf6f50518d894285235
Justin Liman <justinliman97@gmail.com>
2022-06-23 19:47:42 -0700

    implement debounce search

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'a0eb5841',
+            s: searchTerm
+        }
+    });
+    console.log(response);
+    console.log(response.data);
+    return response;
+};
+
+const input = document.querySelector('input');
+
+
+const onInput = (event) => {
+    fetchData(event.target.value);
+};
+
+input.addEventListener('input', debounce(onInput, 1000));
+
+
+
+// let dataa;
+// fetchData().then((d) => {dataa = d});
