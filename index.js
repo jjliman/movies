c6056d9f966c7ea710d8311aecb2317f33a946eb
Justin Liman <justinliman97@gmail.com>
2022-06-24 17:38:02 -0700

    implement stat comparison

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ createAutoComplete({
     root: document.querySelector('#left-autocomplete'),
     onOptionSelect(movie) {
         document.querySelector('.tutorial').classList.add('is-hidden');
-        onMovieSelect(movie, document.querySelector('#left-summary'));
+        onMovieSelect(movie, document.querySelector('#left-summary'), 'left');
     }
 });
 
@@ -40,7 +40,7 @@ createAutoComplete({
     root: document.querySelector('#right-autocomplete'),
     onOptionSelect(movie) {
         document.querySelector('.tutorial').classList.add('is-hidden');
-        onMovieSelect(movie, document.querySelector('#right-summary'));
+        onMovieSelect(movie, document.querySelector('#right-summary'), 'right');
     }
 });
 
@@ -52,22 +52,78 @@ createAutoComplete({
 // let dataa;
 // fetchData().then((d) => {dataa = d});
 
-const onMovieSelect = async (movie, summaryElement) => {
+let leftMovie;
+let rightMovie;
+const onMovieSelect = async (movie, summaryElement, side) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: 'a0eb5841',
             i: movie.imdbID
         }
     });
-    console.log(response);
+    // console.log(response);
 
-    console.log(movieTemplate(response.data));
+    // console.log(movieTemplate(response.data));
     summaryElement.innerHTML = movieTemplate(response.data);
-    console.log(document.querySelector('#summary').innerHTML);
+
+    if (side === 'left') {
+        leftMovie = response.data;
+    } else {
+        rightMovie = response.data;
+    }
+
+    if (leftMovie && rightMovie) {
+        runComparison();
+    }
+};
+
+const runComparison = () => {
+    const leftSideStats = document.querySelectorAll('#left-summary .notification');
+    const rightSideStats = document.querySelectorAll('#right-summary .notification');
+
+    leftSideStats.forEach((leftStat, index) => {
+        const rightStat = rightSideStats[index];
+
+        const leftSideValue = parseFloat(leftStat.dataset.value);
+        const rightSideValue = parseFloat(rightStat.dataset.value);
+
+        console.log(leftSideValue, "vs", rightSideValue);
+
+        // N/A values still broken
+        // if right side is greater than left side
+        if (rightSideValue > leftSideValue) {
+            leftStat.classList.remove('is-primary');
+            leftStat.classList.add('is-warning');
+            rightStat.classList.remove('is-warning');
+            rightStat.classList.add('is-primary');
+        } else { //if left side is greater than right side
+            rightStat.classList.remove('is-primary');
+            rightStat.classList.add('is-warning');
+            leftStat.classList.remove('is-warning');
+            leftStat.classList.add('is-primary');
+        }
+
+    });
+
 };
 
 
 const movieTemplate = (movieDetail) => {
+    const dollars = movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, '');
+    const metascore = movieDetail.Metascore;
+    const imdbRating = movieDetail.imdbRating;
+    const imdbVotes = movieDetail.imdbVotes.replace(/,/g, '');
+    
+    const awards = movieDetail.Awards.split(' ').reduce((acc, word) => {
+        const value = parseInt(word);
+        if (isNaN(value)) {
+            return acc;
+        } else {
+            return acc + value;
+        }
+    }, 0);
+    console.log(dollars, metascore, imdbRating, imdbVotes, awards);
+
     return `
         <article class="media">
             <figure class="media-left">
@@ -83,23 +139,23 @@ const movieTemplate = (movieDetail) => {
                 </div>
             </div>
         </article>
-        <article class="notification is-primary">
+        <article data-value="${awards}" class="notification is-primary">
             <p class="title">${movieDetail.Awards}</p>
             <p class="subtitle">Awards</p>
         </article>
-        <article class="notification is-primary">
+        <article data-value="${dollars}" class="notification is-primary">
             <p class="title">${movieDetail.BoxOffice}</p>
             <p class="subtitle">Box Office</p>
         </article>
-        <article class="notification is-primary">
+        <article data-value="${metascore}" class="notification is-primary">
             <p class="title">${movieDetail.Metascore}</p>
             <p class="subtitle">Metascore</p>
         </article>
-        <article class="notification is-primary">
+        <article data-value="${imdbRating}" class="notification is-primary">
             <p class="title">${movieDetail.imdbRating}</p>
             <p class="subtitle">IMDB Rating</p>
         </article>
-        <article class="notification is-primary">
+        <article data-value="${imdbVotes}" class="notification is-primary">
             <p class="title">${movieDetail.imdbVotes}</p>
             <p class="subtitle">IMDB Votes</p>
         </article>
